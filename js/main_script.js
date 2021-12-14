d351c5e795ab97bed3510afa5308f4ab0685f7f5
Nicholas Pérez <nperez@dannyyesoft.mx>
2021-12-13 19:07:03 -0600

    Inicializacion de proyecto

---
+++ js/main_script.js
@@ -0,0 +1,12 @@
+function principal(){
+    document.getElementById('content').src = "ej_1.html";
+}
+function dibujos(){
+    document.getElementById('content').src = "ej_2.html";
+}
+function textGen(){
+    document.getElementById('content').src = "ej_3.html";
+}
+function diosito(){
+    document.getElementById('content').src = "ej_4.html";
+}
